91fb3f55e511db9b093824f1c53cc5c50d1871fe
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-07-26 13:09:40 -0500

    Update Main.java

--- talleres/taller02/Main.java
+++ talleres/taller02/Main.java
@@ -3,16 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Inventario_1 i1 = new Inventario_1();
-        boolean a, b, c, d;
-        a=i1.SumaGrupo(0, new int[] {2, 4, 8}, 8);
-        b=i1.SumaGrupo(0, new int[] {2, 4, 2}, 8);
-        c=i1.SumaGrupo(0, new int[] {10, 2, 2, 5}, 9);
-        d=i1.SumaGrupo(0, new int[] {10, 2, 2, 5}, 17);
-        if(b)
-        {
-            System.out.println("Congratulations");
-        }
         Inventario_2 p2 = new Inventario_2();
         p2.combinations("Eafit");
     }
