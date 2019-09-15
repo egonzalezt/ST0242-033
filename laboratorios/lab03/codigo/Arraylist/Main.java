7516d158d032c878611eccf78a0691ffc8feeec5
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-09-15 17:54:16 -0500

    Update Main.java

--- laboratorios/lab03/codigo/Arraylist/Main.java
+++ laboratorios/lab03/codigo/Arraylist/Main.java
@@ -12,10 +12,10 @@ public class Main {
         try
         {
             Estudiantes ed1 = new Estudiantes();
-            //llamado del metodo Consultaestudiante para buscar un estudiante en un semestre en especifico
+            //llamado del metodo Consultaestudiante para buscar un estudiante en un semestre en especifico.
             System.out.println("Consultar por estudiante");
             ed1.Consultaestudiante("Abel","20142");
-            //llamado del metodo Consultacurso para buscar un curso y un semestre en especifico
+            //llamado del metodo Consultacurso para buscar un curso y un semestre en especifico.
             System.out.println("Consultar por curso");
             ed1.Consultacurso("FUNDAMENTOS DE PROGRAMACIÓN","20151");
         }
