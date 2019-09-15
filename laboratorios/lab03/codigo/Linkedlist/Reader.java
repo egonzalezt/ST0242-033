f8f074f758db67cd3aec4249c617abed16f6d9fa
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-09-15 16:19:29 -0500

    Se agrega documentación

--- laboratorios/lab03/codigo/Linkedlist/Reader.java
+++ laboratorios/lab03/codigo/Linkedlist/Reader.java
@@ -1,15 +1,23 @@
 package com.company;
 import java.util.Scanner;
 import java.io.*;
+
+/**
+ * Clase Reader, encargada de leer el archivo csv
+ *
+ * @author Esteban Gonzalez Tamayo
+ * @author David Felipe Garcia
+ */
+
 public class Reader {
+    
     Lista Registro;
 
     public Lista Lector()throws FileNotFoundException
     {
+        //creacion de una lista
          Registro = new Lista();
         //lectura del archivo
-        //condicion para registrar mas archivos
-        boolean condicion = true;
         try {
             //creacion del scanner para leer el archivo
             Scanner input = new Scanner(new File("E:\\Desktop\\LAB3\\src\\com\\company\\Libro3.csv"));
@@ -22,6 +30,7 @@ public class Reader {
                 String line = input.nextLine();//reemplaza "" "" por " "
                 //Separa el contenido en un arreglo
                 String[] coleccion = line.split(",");
+                // se agregan los estudiantes a la lista implementando el metodo Addfirst() de la clase lista.
                 this.Registro.Addfirst(new Estudiante(coleccion[0], coleccion[1], coleccion[2], coleccion[3], coleccion[4], coleccion[5], coleccion[6], coleccion[7], coleccion[8],coleccion[9],coleccion[10]));
             }
         } catch(Exception e)
