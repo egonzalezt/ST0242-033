cad1d79c2d735b1919e7914b2226cd6419c83a23
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-09-15 16:12:43 -0500

    Se agrega documentación.

--- laboratorios/lab03/codigo/Linkedlist/Lista.java
+++ laboratorios/lab03/codigo/Linkedlist/Lista.java
@@ -3,13 +3,18 @@ package com.company;
 public class Lista {
     private Nodo cabeza;
     private int size;
-
+    /**
+    * Metodo encargado de crear la cabeza de la lista y establecer el tamaño inicial de la lista.
+    */
     public Lista()
     {
         cabeza = null;
         size=0;
     }
-
+    /**
+    * Metodo encargado de detectar si la cabeza esta vacia.
+    * @return boolean.
+    */
     public boolean Isempty() {
         if (cabeza == null)
         {
@@ -17,37 +22,45 @@ public class Lista {
         }else return false;
     }
 
-    public Object Search(int index)
-    {
-        Nodo temp = cabeza;
-        for(int i=0;i<index;i++)
-        {
-            temp = temp.Obtenersiguiente();
-        }
-        return temp.Obtenerest();
-    }
-
+    /*
+    * Metodo que agrega estudiantes a la cabeza de la lista enlazada.
+    */
     public void Addfirst(Estudiante obj)
     {
+        //verificacion de si la cabeza esta vacia.
         if(cabeza==null)
         {
+            //si esta vacia agrega el nodo que contiene un estudiante a la cabeza.
             cabeza = new Nodo(obj);
         }else
         {
+            //Copia de la cabeza.
             Nodo temp = cabeza;
+            //Nodo que contiene el estudiante.
             Nodo nuevo = new Nodo(obj);
+            //se enlaza el estudiante a la copia de la cabeza.
             nuevo.Enlazarsiguiente(temp);
+            //la cabeza apunta a el estudiante agregado.
             cabeza = nuevo;
         }
+        //aumento del tamaño de la lista.
         size++;
     }
 
+    /**
+    *   Metodo encargado de mostrar el contenido de la lista.
+    */
     public void Verlista()
     {
+        // String que almacena el contenido.
         String texto = "LISTA:\n";
+        // copia de la cabeza.
         Nodo a = cabeza;
+        //indice del iterador.
         int i=0;
+        //ciclo que recorre toda la lista.
         while (i<size){
+            //impresion de la lista invocando el meotodo toString.
             texto = a.Obtenerest().toString() + "\n";
             System.out.println(texto);
             a = a.Obtenersiguiente();
@@ -55,11 +68,19 @@ public class Lista {
         }
     }
 
+    /**
+    *   Metodo encargado de mostrar todos los estudiantes registrados en un curso especifico y semestre solicitado.
+    *   @param materia Materia que se desea filtrar.
+    *   @param semestre Semestre que se desea filtrar.
+    */
+    
     public void Consultacurso(String materia, String semestre)
     {
         String texto;
+        //copia de la cabeza
         Nodo a = cabeza;
         int i=0;
+        //ciclo encargado de recorrer toda la lista y comparar cada elemento de la lista con el nombre y el semestre.
         while (i<size){
             if(a.Obtenerest().getMateria().equalsIgnoreCase(materia)&&a.Obtenerest().getSemestre().equalsIgnoreCase(semestre))
             {
@@ -71,11 +92,19 @@ public class Lista {
         }
     }
 
+    /**
+    *   Metodo encargado de mostrar todas las materias registradas por un estudiante en un semetre en especifico.
+    *   @param nombre Nombre del estudiante.
+    *   @param semestre Semestre que se desea filtrar.
+    */
+    
     public void Consultaestudiante(String nombre, String semestre)
     {
         String texto;
+        //copia de la cabeza
         Nodo a = cabeza;
         int i=0;
+        //ciclo encargado de recorrer toda la lista y comparar cada elemento de la lista con el nombre y el semestre.
         while (i<size){
             if(a.Obtenerest().getNombre().equalsIgnoreCase(nombre)&&a.Obtenerest().getSemestre().equalsIgnoreCase(semestre))
             {
@@ -86,6 +115,11 @@ public class Lista {
             i++;
         }
     }
+    
+    /**
+    * Metodo encargado de mostrar el tamaño de la lista
+    * @return size tamaño de la lista
+    */
     public int getSize()
     {
         return size;
