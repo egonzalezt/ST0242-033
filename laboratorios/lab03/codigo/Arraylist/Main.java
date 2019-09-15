20d668115fd6656f2f91b1bc40c9e42a030f31a6
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-09-15 15:51:48 -0500

    Se agrega documentación

--- laboratorios/lab03/codigo/Arraylist/Main.java
+++ laboratorios/lab03/codigo/Arraylist/Main.java
@@ -2,21 +2,26 @@ package com.company;
 
 import java.util.ArrayList;
 
+/**
+ * @author Esteban Gonzalez Tamayo
+ * @author David Felipe Garcia
+ */
 public class Main {
 
     public static void main(String[] args) {
         try
         {
-            ArrayList<String> cars = new ArrayList<String>();
             Estudiantes ed1 = new Estudiantes();
+            //llamado del metodo Consultaestudiante para buscar un estudiante en un semestre en especifico
             System.out.println("Consultar por estudiante");
             ed1.Consultaestudiante("Abel","20142");
+            //llamado del metodo Consultacurso para buscar un curso y un semestre en especifico
             System.out.println("Consultar por curso");
             ed1.Consultacurso("FUNDAMENTOS DE PROGRAMACIÓN","20151");
         }
         catch (Exception e)
         {
-
+            System.out.println(e);
         }
     }
 }
