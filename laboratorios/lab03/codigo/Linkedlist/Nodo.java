ded14aea99dc1da7c29ce5e90b0d61eb78a5d218
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-09-15 16:16:24 -0500

    Se agrega documentación.

--- laboratorios/lab03/codigo/Linkedlist/Nodo.java
+++ laboratorios/lab03/codigo/Linkedlist/Nodo.java
@@ -1,25 +1,42 @@
 package com.company;
 
 public class Nodo {
+    /**
+    * Atributos de la clase nodo
+    */
     private Estudiante estudiante;
     private Nodo siguiente;
 
+    /*
+    *Constructor de la clase Nodo, almacena un estudiante.
+    */
     public Nodo(Estudiante estudiante)
     {
         this.estudiante = estudiante;
         this.siguiente= null;
     }
 
+    /**
+    * Metodo encargado de retornar el estudiante del nodo
+    * @return estudiante
+    */
     public Estudiante Obtenerest()
     {
         return this.estudiante;
     }
-
+    
+    /**
+    * Metodo que enlaza el nodo actual a otro nodo 
+    */
     public void Enlazarsiguiente(Nodo siguiente)
     {
         this.siguiente = siguiente;
     }
 
+    /*
+    * Metodo que retorna el nodo que fue enlazado
+    * @return siguiente
+    */
     public Nodo Obtenersiguiente()
     {
         return siguiente;
