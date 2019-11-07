d897d9e0fdae4f2ea8469b4ab98505633650e584
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-11-07 17:17:48 -0500

    Update Decisiontree.py

--- proyecto/codigo/Decisiontree.py
+++ proyecto/codigo/Decisiontree.py
@@ -42,32 +42,6 @@ def Decisiontree():
     Indep = data[variables].values 
 
     Dep = data['label'].values
-
-    # Implementacion del arbol de decision con el criterio de 'GINI', con una profundidad maxima de 4 nodos y finalmente un minimo de 5 hojas en el arbol
-
-    Arbol = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=22, min_samples_leaf=5)
-
-    # Entrenamiento del arbol de decision para poder detectar cual sera la variable que mas parte el arbol y genera el indice de gini mas bajo.
-    # este requiere los valores independientes y dependientes para ser analizado.
-
-    Arbol.fit(Indep, Dep) 
-
-    print("Generando imagen del arbol \n")
-
-    # Implementacion de Grapviz para la graficacion del arbol.
-    # filled y rounded se implementan para que el arbol tenga un color y detalle para diferenciar cuando hay o no hay roya, adicionalmente    'class_names' contiene los
-    # valores que digitaran si hay o no hay roya.
-
-    dot_data = export_graphviz( Arbol,filled=True, rounded=True,class_names=['yes', 'no'],feature_names=variables)
-
-    # Importacion del arbol generado en graphviz por medio de un archivo de tipo '.png' el cual contendra el arbol generado.
-    
-    graph = graph_from_dot_data(dot_data)                 
-    graph.write_png('tree.png')   
-    print("Imagen generada\n")
-
-    # Implementacion de try para evitar errores que pueden ser ocasionados al digitar valores erroneos como tipo string 
-
     try:
         print(" Digite los valores separados por comas a evaluar: \n")
         print("ph  soil_temperature  soil_moisture  illuminance  env_temperature  env_humidity")
@@ -84,13 +58,6 @@ def Decisiontree():
         # Esta parte del codigo se implementara para predecir el valor previamente digitado y almacenado en un arreglo para detectar 
         # si el valor digitado tiene o no roya por lo cual para realizar este paso es necesario primero entrenar al arbol para que pueda detectar el patron.
 
-        Prediction = Arbol.predict([PredicVals])
-
-        print("\n Prediccion con: \n\n", PredicVals,end=" -> ")
-
-        if Prediction == 'yes':
-            print("El valor ingresado posee roya")
-        else: print("El valor ingresado no posee roya")
     except ValueError:
         print ("Error! Valor no valido. Intentelo otra vez...")
 
@@ -120,11 +87,7 @@ def Exetime():
 
     # Toma de tiempos para la creacion del arbol
     time3 = time.time()
-    variables = ['ph', 'soil_temperature', 'soil_moisture', 'illuminance','env_temperature', 'env_humidity']                   
-    Indep = data[variables].values 
-    Dep = data['label'].values
-    Arbol = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=4, min_samples_leaf=5)
-    time4 = time.time()
+
     print("Tiempo de creacion del arbol: \n -> ", time4-time3,"s \n")
 
     # Toma de tiempos para el entrenamiento del arbol con los datos obtenidos en el csv
@@ -132,46 +95,6 @@ def Exetime():
     Arbol.fit(Indep, Dep) 
     time6 = time.time()
     print("Tiempo de entrenamiento del arbol: \n -> ", time6-time5,"s \n")
-
-    # Toma de tiempos para la graficacion del arbol de decisiones 
-    time7 = time.time()
-    dot_data = export_graphviz( Arbol,filled=True, rounded=True,class_names=['yes', 'no'],feature_names=variables)
-    graph = graph_from_dot_data(dot_data)                 
-    graph.write_png('time.png')   
-    time8 = time.time()
-    print("Tiempo de graficacion del arbol: \n -> ", time8-time7,"s \n")
-
-    # Toma de tiempos para la prediccion de un valor ingresado 
-    try:
-        print(" Digite los valores separados por comas a evaluar: \n")
-        print("ph  soil_temperature  soil_moisture  illuminance  env_temperature  env_humidity")
-
-        # Lectura de los valores digitados en la consola, estos seran procesados para luego ser evaluados.
-        # Esta parte del codigo leera los valores digitados para despues almacenarlos en un arreglo que sera implementado futuramente.
-
-        str = input("-> ")
-        list = str.split (",")
-        time9 = time.time()
-        PredicVals = []
-        for i in list:
-    	    PredicVals.append(float(i))
-
-        # Esta parte del codigo se implementara para predecir el valor previamente digitado y almacenado en un arreglo para detectar 
-        # si el valor digitado tiene o no roya por lo cual para realizar este paso es necesario primero entrenar al arbol para que pueda detectar el patron.
-
-        Prediction = Arbol.predict([PredicVals])
-
-        print("\n Prediccion con: \n\n", PredicVals,end=" -> ")
-
-        if Prediction == 'yes':
-            print("El valor ingresado posee roya")
-        else: print("El valor ingresado no posee roya")
-
-        time10 = time.time()
-        print("\n Tiempo de prediccion con los datos: \n -> ", time10-time9,"s \n")
-
-    except ValueError:
-        print ("Error! Valor no valido. Intentelo otra vez...")
 
 if __name__ == '__main__':
 
